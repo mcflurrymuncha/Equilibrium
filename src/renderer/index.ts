4a8963ad06e6e6c1b9f7bdfc6417165294e12e8c
mcflurry <vexithereal@gmail.com>
2026-05-01 17:31:43 +0100

    Update index.ts

--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -31,14 +31,14 @@ const { customEntries, customSections } = Vencord.Plugins.plugins.Settings as an
 
 customEntries.push({
     key: "equicord_equibop_settings",
-    title: "Equibop Settings",
+    title: "Equicordium Settings",
     Component: SettingsUi,
     Icon: VesktopSettingsIcon
 });
 
 customSections.push(() => ({
     section: "EquibopSettings",
-    label: "Equibop Settings",
+    label: "Equilibrium Settings",
     element: SettingsUi,
     className: "vc-equibop-settings"
 }));
